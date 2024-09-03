1c98d85c9018225bb2e2c2744bfe57eab7cfbc76
Your Name <you@example.com>
2024-09-03 11:07:26 +0530

    first commit

---
+++ data/test/emotion (2).py
@@ -0,0 +1,44 @@
+from keras.models import load_model 
+from time import sleep 
+from keras.preprocessing.image import img_to_array 
+from keras.preprocessing import image
+import cv2 
+import numpy as np
+face classifier = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+classifier load_model('./Emotion Detection.h5')
+class_labels = ['Angry', 'Happy', 'Neutral', 'Sad', 'Surprise']
+cap = cv2.VideoCapture(0)
+while True: 
+     # Grab a single frame of video  18  ret, frame cap.read()
+     ret,frame = ret, frame = cap.read()
+     labels = []
+     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+     #detect the face
+     faces = face classifier.detectMultiScale(gray,1.3,5) 
+     for (x,y,w,h) in faces:
+        #boundary of face
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0),2)
+        roi_gray = gray[y:y+h,x:x+w] 
+        #mobile net arch should be gie input image 48x48 
+        roi_gray = cv2.resize(roi_gray, (48,48), interpolation-cv2.INTER_AREA)
+        #converting the image into the array 
+        if np.sum([roi_gray])!=0:
+            roi = roi_gray.astype('float')/255.0
+            roi = img_to_array(roi) 
+            roi = np.expand_dims (roi, axis=0)  
+            # make a prediction  preds = classifier.predict(roi)[0]  
+            preds = classifier.predict(roi)[0]
+            print("\nprediction", preds)  
+            label-class labels [preds.argmax()]  
+            print("\nprediction max = ", preds.argmax())  
+            print("\nlabel ",label)  label_position= (x,y)  
+            cv2.putText(frame, label, label_position,cv2.FONT HERSHEY SIMPLEX, 2, (0,255,0),3)
+            else:  
+                cv2.putText(frame, 'No Face Found', (20,60), cv2. FONT HERSHEY SIMPLEX ,2,(0,255,0),3)
+            print("\n\n")
+        cv2.imshow('Emotion Detector',frame)
+        if cv2.waitkey(1) & 0*FF == ord('q'):
+            break
+cap.release()
+cv2.destroyAllWindows()
+        
---
+++ testdataset.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from keras.models import load_model
+import glob
+import random
+import pygame
+
+model = load_model('model_file_30epochs.h5')
+faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+labels_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
+cam = cv2.VideoCapture(0)
+ret, frame = cam.read()
+
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+faces = faceDetect.detectMultiScale(gray, 1.3, 3)
+label = 4
+
+for x, y, w, h in faces:
+    sub_face_img = gray[y:y + h, x:x + w]
+    resized = cv2.resize(sub_face_img, (48, 48))
+    normalize = resized / 255.0
+    reshaped = np.reshape(normalize, (1, 48, 48, 1))
+    result = model.predict(reshaped)
+    label = np.argmax(result, axis=1)[0]
+    print("label", label)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
+    cv2.rectangle(frame, (x, y - 40), (x + w, y), (50, 50, 255), -1)
+    cv2.putText(frame, labels_dict[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+
+# cv2.imshow("Frame", frame)
+
+cv2.imwrite("capture.jpg".format(1), frame)
+import os
+os.system("capture.jpg")
+# Release the camera
+cam.release()
+
+emotion_folders = {
+    0: 'Angry',
+    1: 'Disgust',
+    2: 'Fear',
+    3: 'Happy',
+    4: 'Neutral',
+    5: 'Sad',
+    6: 'Surprise'
+}
+
+emotion_folder = emotion_folders[label]
+songs_folder = f"songs/{emotion_folder}/"
+
+# Get a random song from the selected emotion folder
+songs = glob.glob(os.path.join(songs_folder, '*.mp3'))
+if songs:
+    random_song = random.choice(songs)
+    pygame.mixer.init()
+    pygame.mixer.music.load(random_song)
+    pygame.mixer.music.play()
+
+    # Wait for the song to finish or stop after a certain duration
+    pygame.time.wait(500000)  # You can adjust the duration or remove this line
+
+    # Stop all sound playback
+    pygame.mixer.stop()
+
+else:
+    print(f"No songs found in {emotion_folder} folder.")
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
